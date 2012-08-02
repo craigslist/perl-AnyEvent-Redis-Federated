672ec492e0a40962176b05b09b4a8481133fcde4
Jeremy Zawodny <jzawodn@craigslist.org>
2012-08-01 17:02:34 -0700

    0.05 and fix for Makefile.PL

--- lib/AnyEvent/Redis/Federated.pm
+++ lib/AnyEvent/Redis/Federated.pm
@@ -12,7 +12,7 @@ use Set::ConsistentHash;   # for hash ring logic
 use Digest::MD5 qw(md5);   # for hashing keys
 use Scalar::Util qw(weaken);
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 # keep a global object cache that will contain weak references to
 # objects keyed on their tag.  this allows for sharing of objects
